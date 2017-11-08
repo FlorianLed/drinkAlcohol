7e29813b0bcb9076e23fe9e8ce3b8ad5880a6ae4
Florian Ledrich <florianledrich@hotmail.com>
2017-11-08 14:07:29 +0100

    routing mis en place entre page d'accueil et connexion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { AccueilComponent } from './accueil/accueil.component';
 
+
+const routes: Routes= [
+  {
+    path: "accueil", component: AccueilComponent
+  },
+  {
+    path: "header", component: HeaderComponent
+  },
+  {
+    path: "connexion", component: ConnexionComponent
+  },
+  {
+    path: "", redirectTo: "/accueil", pathMatch: "full"
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ConnexionComponent,
+    AccueilComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

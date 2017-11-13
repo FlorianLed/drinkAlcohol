f357a794109cc9962e97bdd9ec5b527288a70656
Viththakhan <vitasati@gmail.com>
2017-11-13 12:40:22 +0100

    Merge branch 'master' of https://github.com/FlorianLed/drinkAlcohol
    # Conflicts:
    #	src/app/connexion/connexion.component.html

--- src/app/utilisateur.ts
+++ src/app/utilisateur.ts
@@ -1,77 +1,62 @@
 export class Utilisateur {
   private _nom: string;
   private _prenom: string;
-  //private _dateNaissance: ;
-  private _codePostal: number;
+  private _dateNaissance: string;
   private _ville: string;
+  private _cp: number;
   private _rue: string;
-  private _numero: number;
-  private _email: string;
+  private _num: number;
   private _gsm: number;
-  private _pseudo: string;
-  private _motDePasse: string;
+  private _mail: string;
+  private _pseudos: string;
+  private _mp: string;
+  private _cmp: string;
 
+  constructor(nom: string = '',prenom: string = '',dateNaissance: string = '',ville: string = '',cp: number = 0,rue: string = '',
+              num: number = 0,gsm: number = 0,mail: string = '',pseudos: string = '',mp: string = '',cmp: string = ''){
 
-
-
-
-  get nom(): string {
-    return this._nom;
+    this._nom=nom;
+    this._prenom=prenom;
+    this._dateNaissance=dateNaissance;
+    this._ville=ville;
+    this._cp=cp;
+    this._rue=rue;
+    this._num=num;
+    this._gsm=gsm;
+    this._mail=mail;
+    this._pseudos=pseudos;
+    this._mp=mp;
+    this._cmp=cmp;
   }
 
-  set nom(value: string) {
-    this._nom = value;
+  get cmp(): string {
+    return this._cmp;
   }
 
-  get prenom(): string {
-    return this._prenom;
+  set cmp(value: string) {
+    this._cmp = value;
+  }
+  get mp(): string {
+    return this._mp;
   }
 
-  set prenom(value: string) {
-    this._prenom = value;
+  set mp(value: string) {
+    this._mp = value;
+  }
+  get pseudos(): string {
+    return this._pseudos;
   }
 
-
-  get codePostal(): number {
-    return this._codePostal;
+  set pseudos(value: string) {
+    this._pseudos = value;
+  }
+  get mail(): string {
+    return this._mail;
   }
 
-  set codePostal(value: number) {
-    this._codePostal = value;
+  set mail(value: string) {
+    this._mail = value;
   }
-
-  get ville(): string {
-    return this._ville;
-  }
-
-  set ville(value: string) {
-    this._ville = value;
-  }
-
-  get rue(): string {
-    return this._rue;
-  }
-
-  set rue(value: string) {
-    this._rue = value;
-  }
-
-  get numero(): number {
-    return this._numero;
-  }
-
-  set numero(value: number) {
-    this._numero = value;
-  }
-
-  get email(): string {
-    return this._email;
-  }
-
-  set email(value: string) {
-    this._email = value;
-  }
-
   get gsm(): number {
     return this._gsm;
   }
@@ -79,20 +64,53 @@ export class Utilisateur {
   set gsm(value: number) {
     this._gsm = value;
   }
-
-  get pseudo(): string {
-    return this._pseudo;
+  get num(): number {
+    return this._num;
   }
 
-  set pseudo(value: string) {
-    this._pseudo = value;
+  set num(value: number) {
+    this._num = value;
+  }
+  get rue(): string {
+    return this._rue;
   }
 
-  get motDePasse(): string {
-    return this._motDePasse;
+  set rue(value: string) {
+    this._rue = value;
+  }
+  get cp(): number {
+    return this._cp;
   }
 
-  set motDePasse(value: string) {
-    this._motDePasse = value;
+  set cp(value: number) {
+    this._cp = value;
+  }
+  get ville(): string {
+    return this._ville;
+  }
+
+  set ville(value: string) {
+    this._ville = value;
+  }
+  get dateNaissance(): string {
+    return this._dateNaissance;
+  }
+
+  set dateNaissance(value: string) {
+    this._dateNaissance = value;
+  }
+  get prenom(): string {
+    return this._prenom;
+  }
+
+  set prenom(value: string) {
+    this._prenom = value;
+  }
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
   }
 }
